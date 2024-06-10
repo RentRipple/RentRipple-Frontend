797879d2da43489894122f29001a69e36903696f
meet57 <meet57.mp@gmail.com>
2024-06-10 18:06:02 -0400

    Page Done

--- src/views/Register/Register.jsx
+++ src/views/Register/Register.jsx
@@ -1,193 +1,364 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
-  TextField,
   Button,
+  Stepper,
+  Step,
+  StepLabel,
   Typography,
-  Link,
+  TextField,
   Grid,
   MenuItem,
 } from "@mui/material";
-import { styled } from "@mui/system";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
-import { Link as RouterLink } from "react-router-dom";
+import sampleImage from "../../assets/signup.jpg"; // Adjust the path to your image file
 
-const FormContainer = styled(Box)(() => ({
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  padding: "20px",
-}));
+const steps = [
+  "Personal Details",
+  "Contact Details",
+  "Account Details",
+  "Security Questions",
+];
 
-const LoginButton = styled(Button)(() => ({
-  marginTop: "20px",
-  backgroundColor: "#04c4cc",
-  color: "#fff",
-  "&:hover": {
-    backgroundColor: "#9dcccf",
-  },
-}));
+const validationSchema = [
+  Yup.object({
+    firstname: Yup.string().required("Required"),
+    lastname: Yup.string().required("Required"),
+  }),
+  Yup.object({
+    gender: Yup.string()
+      .oneOf(["Male", "Female", "Other"])
+      .required("Required"),
+    accountType: Yup.string().oneOf(["Tenant", "Owner"]).required("Required"),
+    number: Yup.string()
+      .matches(
+        /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
+        "Phone number is not valid"
+      )
+      .min(10, "Phone number is not valid")
+      .max(10, "Phone number is not valid"),
+  }),
+  Yup.object({
+    email: Yup.string().email("Invalid email address").required("Required"),
+    password: Yup.string().required("Required"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Passwords must match")
+      .required("Required"),
+    
+  }),
+  Yup.object({
+    answer1: Yup.string().required("Required"),
+    answer2: Yup.string().required("Required"),
+    answer3: Yup.string().required("Required"),
+  }),
+];
 
-const phoneRegExp =
-  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
-
-const validationSchema = Yup.object({
-  firstname: Yup.string().required("Required"),
-  lastname: Yup.string().required("Required"),
-  username: Yup.string().required("Required"),
-  gender: Yup.string().oneOf(["Male", "Female", "Other"]).required("Required"),
-  number: Yup.string()
-    .matches(phoneRegExp, "Phone number is not valid")
-    .min(10, "Phone number is not valid")
-    .max(10, "Phone number is not valid"),
-  email: Yup.string().email("Invalid email address").required("Required"),
-  password: Yup.string().required("Required"),
-  accountType: Yup.string().oneOf(["Tenant", "Owner"]).required("Required"),
-});
+const initialValues = {
+  firstname: "",
+  lastname: "",
+  gender: "Male",
+  number: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+  accountType: "Tenant",
+  question1: "",
+  answer1: "",
+  question2: "",
+  answer2: "",
+  question3: "",
+  answer3: "",
+};
 
 const Register = () => {
+  const [activeStep, setActiveStep] = useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const questions = [
+    "What's your favorite city?",
+    "What's your favorite sport?",
+    "What's your favorite color?",
+    "What's your father's middle name?",
+    "What's your mother's maiden name?",
+    "What's your favorite pet?",
+  ];
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
   return (
-    <Grid id="Meet" container justifyContent="center" gap={2}>
+    <Grid container justifyContent="center" gap={2}>
+      <Grid item xs={12} sm={6} md={4}>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="100%"
+        >
+          <img
+            src={sampleImage}
+            alt="Sample"
+            style={{
+              maxWidth: "300%",
+              maxHeight: "400px",
+              objectFit: "contain",
+            }}
+          />
+        </Box>
+      </Grid>
       <Grid item xs={12} sm={6} md={6}>
+        <Stepper activeStep={activeStep} alternativeLabel>
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
         <Formik
-          initialValues={{
-            firstname: "",
-            lastname: "",
-            username: "",
-            gender: "",
-            number: "",
-            email: "",
-            password: "",
-            accountType: "",
-          }}
-          validationSchema={validationSchema}
+          initialValues={initialValues}
+          validationSchema={validationSchema[activeStep]}
           onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
+            if (activeStep === steps.length - 1) {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            } else {
+              handleNext();
               setSubmitting(false);
-            }, 400);
+            }
           }}
         >
           {({ isSubmitting, errors, touched }) => (
             <Form>
-              <FormContainer>
-                <Typography variant="h4" component="h1" gutterBottom>
-                  Register
-                </Typography>
-                <Typography variant="body2" component="p" gutterBottom>
-                  Do you have an account yet?{" "}
-                  <Link component={RouterLink} to="/login">
-                    Sign in
-                  </Link>
-                </Typography>
-                <Field
-                  as={TextField}
-                  name="firstname"
-                  type="text"
-                  label="First Name"
-                  variant="outlined"
-                  fullWidth
-                  margin="normal"
-                  error={touched.firstname && !!errors.firstname}
-                  helperText={touched.firstname && errors.firstname}
-                />
-                <Field
-                  as={TextField}
-                  name="lastname"
-                  type="text"
-                  label="Last Name"
-                  variant="outlined"
-                  fullWidth
-                  margin="normal"
-                  error={touched.lastname && !!errors.lastname}
-                  helperText={touched.lastname && errors.lastname}
-                />
-                <Field
-                  as={TextField}
-                  name="username"
-                  type="text"
-                  label="Username"
-                  variant="outlined"
-                  fullWidth
-                  margin="normal"
-                  error={touched.username && !!errors.username}
-                  helperText={touched.username && errors.username}
-                />
-                <Field
-                  as={TextField}
-                  name="gender"
-                  select
-                  label="Gender"
-                  variant="outlined"
-                  fullWidth
-                  margin="normal"
-                  error={touched.gender && !!errors.gender}
-                  helperText={touched.gender && errors.gender}
-                >
-                  <MenuItem value="Male">Male</MenuItem>
-                  <MenuItem value="Female">Female</MenuItem>
-                  <MenuItem value="Other">Other</MenuItem>
-                </Field>
-                <Field
-                  as={TextField}
-                  name="number"
-                  type="number"
-                  label="Number"
-                  variant="outlined"
-                  fullWidth
-                  margin="normal"
-                  error={touched.number && !!errors.number}
-                  helperText={touched.number && errors.number}
-                />
-                <Field
-                  as={TextField}
-                  name="email"
-                  type="email"
-                  label="Email"
-                  variant="outlined"
-                  fullWidth
-                  margin="normal"
-                  error={touched.email && !!errors.email}
-                  helperText={touched.email && errors.email}
-                />
-                <Field
-                  as={TextField}
-                  name="password"
-                  type="password"
-                  label="Password"
-                  variant="outlined"
-                  fullWidth
-                  margin="normal"
-                  error={touched.password && !!errors.password}
-                  helperText={touched.password && errors.password}
-                />
-                <Field
-                  as={TextField}
-                  name="accountType"
-                  select
-                  label="Account Type"
-                  variant="outlined"
-                  fullWidth
-                  margin="normal"
-                  error={touched.accountType && !!errors.accountType}
-                  helperText={touched.accountType && errors.accountType}
-                >
-                  <MenuItem value="Tenant">Tenant</MenuItem>
-                  <MenuItem value="Owner">Owner</MenuItem>
-                </Field>
-                <LoginButton
+              {activeStep === 0 && (
+                <>
+                  <Field
+                    as={TextField}
+                    name="firstname"
+                    type="text"
+                    label="First Name"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    error={touched.firstname && !!errors.firstname}
+                    helperText={touched.firstname && errors.firstname}
+                  />
+                  <Field
+                    as={TextField}
+                    name="lastname"
+                    type="text"
+                    label="Last Name"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    error={touched.lastname && !!errors.lastname}
+                    helperText={touched.lastname && errors.lastname}
+                  />
+                </>
+              )}
+              {activeStep === 1 && (
+                <>
+                  <Field
+                    as={TextField}
+                    name="gender"
+                    select
+                    label="Gender"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    error={touched.gender && !!errors.gender}
+                    helperText={touched.gender && errors.gender}
+                  >
+                    <MenuItem value="Male">Male</MenuItem>
+                    <MenuItem value="Female">Female</MenuItem>
+                    <MenuItem value="Other">Other</MenuItem>
+                  </Field>
+                  <Field
+                    as={TextField}
+                    name="accountType"
+                    select
+                    label="Account Type"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    error={touched.accountType && !!errors.accountType}
+                    helperText={touched.accountType && errors.accountType}
+                  >
+                    <MenuItem value="Tenant">Tenant</MenuItem>
+                    <MenuItem value="Owner">Owner</MenuItem>
+                  </Field>
+                  <Field
+                    as={TextField}
+                    name="number"
+                    type="text"
+                    label="Number"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    error={touched.number && !!errors.number}
+                    helperText={touched.number && errors.number}
+                  />
+                </>
+              )}
+              {activeStep === 2 && (
+                <>
+                  <Field
+                    as={TextField}
+                    name="email"
+                    type="email"
+                    label="Email"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    error={touched.email && !!errors.email}
+                    helperText={touched.email && errors.email}
+                  />
+                  <Field
+                    as={TextField}
+                    name="password"
+                    type="password"
+                    label="Password"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    error={touched.password && !!errors.password}
+                    helperText={touched.password && errors.password}
+                  />
+                  <Field
+                    as={TextField}
+                    name="confirmPassword"
+                    type="password"
+                    label="Confirm Password"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    error={touched.confirmPassword && !!errors.confirmPassword}
+                    helperText={
+                      touched.confirmPassword && errors.confirmPassword
+                    }
+                  />
+                </>
+              )}
+              {activeStep === 3 && (
+                <>
+                  <Field
+                    as={TextField}
+                    name="question1"
+                    select
+                    label="Security Question 1"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    error={touched.question1 && !!errors.question1}
+                    helperText={touched.question1 && errors.question1}
+                  >
+                    {questions.map((question, index) => (
+                      <MenuItem key={index} value={question}>
+                        {question}
+                      </MenuItem>
+                    ))}
+                  </Field>
+                  <Field
+                    as={TextField}
+                    name="answer1"
+                    type="text"
+                    label="Answer"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    error={touched.answer1 && !!errors.answer1}
+                    helperText={touched.answer1 && errors.answer1}
+                  />
+                  <Field
+                    as={TextField}
+                    name="question2"
+                    select
+                    label="Security Question 2"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    error={touched.question2 && !!errors.question2}
+                    helperText={touched.question2 && errors.question2}
+                  >
+                    {questions.map((question, index) => (
+                      <MenuItem key={index} value={question}>
+                        {question}
+                      </MenuItem>
+                    ))}
+                  </Field>
+                  <Field
+                    as={TextField}
+                    name="answer2"
+                    type="text"
+                    label="Answer"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    error={touched.answer2 && !!errors.answer2}
+                    helperText={touched.answer2 && errors.answer2}
+                  />
+                  <Field
+                    as={TextField}
+                    name="question3"
+                    select
+                    label="Security Question 3"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    error={touched.question3 && !!errors.question3}
+                    helperText={touched.question3 && errors.question3}
+                  >
+                    {questions.map((question, index) => (
+                      <MenuItem key={index} value={question}>
+                        {question}
+                      </MenuItem>
+                    ))}
+                  </Field>
+                  <Field
+                    as={TextField}
+                    name="answer3"
+                    type="text"
+                    label="Answer"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    error={touched.answer3 && !!errors.answer3}
+                    helperText={touched.answer3 && errors.answer3}
+                  />
+                </>
+              )}
+              <Box display="flex" justifyContent="space-between" marginTop={2}>
+                <Button disabled={activeStep === 0} onClick={handleBack}>
+                  Back
+                </Button>
+                <Button
                   type="submit"
                   variant="contained"
-                  fullWidth
+                  color="primary"
                   disabled={isSubmitting}
                 >
-                  Register
-                </LoginButton>
-              </FormContainer>
+                  {activeStep === steps.length - 1 ? "Finish" : "Next"}
+                </Button>
+              </Box>
             </Form>
           )}
         </Formik>
+        {activeStep === steps.length && (
+          <Box textAlign="center" marginTop={2}>
+            <Typography variant="h6">All steps completed</Typography>
+            <Button onClick={handleReset}>Reset</Button>
+          </Box>
+        )}
       </Grid>
     </Grid>
   );
